a245dc2a30384d6a855c3f801dc4581b8ad7ae55
reqvi <slavil.larin@gmail.com>
2026-06-05 21:02:19 +0200

    Add NoteService.cs and implement CreateNoteAsync method to handle POST/notes (create note)

---
+++ NoteManager.API/Services/INoteService.cs
@@ -0,0 +1,8 @@
+﻿using NoteManager.Dtos;
+
+namespace NoteManager.Services;
+
+public interface INoteService
+{
+    Task<NoteDto> CreateNoteAsync(CreateNoteDto dto);
+}
---
+++ NoteManager.API/Services/NoteService.cs
@@ -0,0 +1,37 @@
+﻿using NoteManager.Data;
+using NoteManager.Dtos;
+using NoteManager.Models;
+
+namespace NoteManager.Services;
+
+public class NoteService : INoteService
+{
+    private readonly AppDbContext _appDbContext;
+
+    public NoteService(AppDbContext appDbContext)
+    {
+        _appDbContext = appDbContext;
+    }
+
+    public async Task<NoteDto> CreateNoteAsync(CreateNoteDto dto)
+    {
+        var note = new Note()
+        {
+            Title = dto.Title,
+            Content = dto.Content,
+            CreatedAt = DateTime.UtcNow
+        };
+
+        _appDbContext.Notes.Add(note);
+
+        await _appDbContext.SaveChangesAsync();
+
+        return new NoteDto()
+        {
+            Id = note.Id,
+            Title = note.Title,
+            Content = note.Content,
+            CreatedAt = note.CreatedAt
+        };
+    }
+}
