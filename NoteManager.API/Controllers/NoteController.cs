12aa6a873e0de14b169a535d1243b0cadb106b5b
reqvi <slavil.larin@gmail.com>
2026-06-05 21:05:37 +0200

    Implement CreateNote method in NoteController.cs to handle POST (/notes) requests. Add scopes to Program.cs to support dependency injection.

--- NoteManager.API/Controllers/NoteController.cs
+++ NoteManager.API/Controllers/NoteController.cs
@@ -1,13 +1,27 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
 using NoteManager.Dtos;
+using NoteManager.Services;
 
 namespace NoteManager.Controllers;
 
 [ApiController]
-public class NoteController
+
+public class NoteController : ControllerBase
 {
+    private readonly INoteService _service;
+
+    public NoteController(INoteService service)
+    {
+        _service = service;
+    }
+    
+    [HttpPost]
+    [Route("/notes")]
     public async Task<IActionResult> CreateNote([FromBody] CreateNoteDto dto)
     {
-        
+        var createdNote = await _service.CreateNoteAsync(dto);
+
+        return Ok(createdNote);
     }
 }
--- NoteManager.API/Program.cs
+++ NoteManager.API/Program.cs
@@ -1,9 +1,21 @@
+using Microsoft.EntityFrameworkCore;
+using NoteManager.Data;
+using NoteManager.Services;
+
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseNpgsql(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<INoteService, NoteService>();
 
 var app = builder.Build();
 
 
 app.UseHttpsRedirection();
 
+app.MapControllers();
 
 app.Run();
